41e7962c24318672a3abd90962b3836bd0338943
Andrea Santoro <andrea.santoro@epfl.ch>
2022-10-26 15:09:47 +0200

    Update launch_High_order_TS_with_edgesaving.sh

--- Example/launch_High_order_TS_with_edgesaving.sh
+++ Example/launch_High_order_TS_with_edgesaving.sh
@@ -6,6 +6,6 @@ codepath="../High_order_TS/simplicial_multivariate.py"
 filename="./../Kaneko_CLM/trial_N50_T240_r175_eps012_008_03_0068_005.txt_kaneko"
 outputfile_edges="../Sample_results/edges_projection"
 outputfile="../Sample_results/results_T0_5.txt"
-python ${codepath} ${filename} -t 0 5 -p 5 -s ${outputfile_edges} > ${outputfile}
+python ${codepath} ${filename} -t 0 5 -p 5 -s ${outputfile_edges}
 
 
